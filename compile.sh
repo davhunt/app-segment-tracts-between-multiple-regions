364ecf87c1965f487ed3eaec31a9e1e8cb397fbf
Daniel Bullock <dnbulloc@i5.red.uits.iu.edu>
2018-12-05 18:27:16 -0500

    first commit of seg app code

---
+++ compile.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+module unload matlab
+module load matlab/2017a
+
+log=compiled/commit_ids.txt
+true > $log
+echo "/N/u/brlife/git/vistasoft" >> $log
+(cd /N/u/brlife/git/vistasoft && git log -1) >> $log
+echo "/N/u/brlife/git/jsonlab" >> $log
+(cd /N/u/brlife/git/jsonlab && git log -1) >> $log
+echo "/N/u/brlife/git/wma_tools" >> $log
+(cd /N/u/brlife/git/wma_tools && git log -1) >> $log
+echo "/N/u/brlife/git/encode" >> $log
+(cd /N/u/brlife/git/encode && git log -1) >> $log
+#echo "/N/u/brlife/git/spm12" >> $log
+#(cd /N/u/brlife/git/spm12 && git log -1) >> $log
+
+cat > build.m <<END
+addpath(genpath('/N/u/brlife/git/vistasoft'))
+addpath(genpath('/N/u/brlife/git/jsonlab'))
+addpath(genpath('/N/u/brlife/git/wma_tools'))
+mcc -m -R -nodisplay -d compiled bsc_SegROIfromPairStringList_BL
+exit
+END
+matlab -nodisplay -nosplash -r build && rm build.m
+
