907386d6d11174a20e5172b8a99cd61768016ed5
Daniel Bullock <dnbulloc@iu.edu>
2019-04-16 12:44:19 -0400

    recompiled with mrtrix path added to compile code

--- compile.sh
+++ compile.sh
@@ -19,6 +19,8 @@ cat > build.m <<END
 addpath(genpath('/N/u/brlife/git/vistasoft'))
 addpath(genpath('/N/u/brlife/git/jsonlab'))
 addpath(genpath('/N/u/brlife/git/wma_tools'))
+addpath(genpath('/N/soft/rhel7/mrtrix/3.0/mrtrix3/matlab'))
+
 mcc -m -R -nodisplay -d compiled bsc_SegROIfromPairStringList_BL
 exit
 END
