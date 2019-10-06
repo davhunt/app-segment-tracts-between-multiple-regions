adbe9c56c3b7fd075c85ef4f41f0c0973b56c6e6
David Hunt <davhunt@indiana.edu>
2019-10-06 00:20:15 -0400

    troubleshoot atlas dir

--- compile.sh
+++ compile.sh
@@ -8,8 +8,10 @@ echo "/N/u/brlife/git/vistasoft" >> $log
 (cd /N/u/brlife/git/vistasoft && git log -1) >> $log
 echo "/N/u/brlife/git/jsonlab" >> $log
 (cd /N/u/brlife/git/jsonlab && git log -1) >> $log
-echo "/N/u/brlife/git/wma_tools" >> $log
-(cd /N/u/brlife/git/wma_tools && git log -1) >> $log
+#echo "/N/u/brlife/git/wma_tools" >> $log
+#(cd /N/u/brlife/git/wma_tools && git log -1) >> $log
+echo "/N/u/davhunt/Carbonate/wma_tools" >> $log
+(cd /N/u/davhunt/Carbonate/wma_tools && git log -1) >> $log
 echo "/N/u/brlife/git/encode" >> $log
 (cd /N/u/brlife/git/encode && git log -1) >> $log
 #echo "/N/u/brlife/git/spm12" >> $log
@@ -18,10 +20,10 @@ echo "/N/u/brlife/git/encode" >> $log
 cat > build.m <<END
 addpath(genpath('/N/u/brlife/git/vistasoft'))
 addpath(genpath('/N/u/brlife/git/jsonlab'))
-addpath(genpath('/N/u/brlife/git/wma_tools'))
+addpath(genpath('/N/u/davhunt/Carbonate/wma_tools'))
 addpath(genpath('/N/soft/rhel7/mrtrix/3.0/mrtrix3/matlab'))
 
-mcc -m -R -nodisplay -d compiled bsc_SegROIfromPairStringList_BL
+mcc -m -R -nodisplay -d compiled bsc_SegROIfromPairStringList_v2_BL
 exit
 END
 matlab -nodisplay -nosplash -r build && rm build.m
